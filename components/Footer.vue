eb97a10228647788a5ed21704123227ee1688235
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2022-12-21 23:14:15 +0100

    comment maps cause bug

--- components/Footer.vue
+++ components/Footer.vue
@@ -14,7 +14,9 @@
       <p>03 58 45 60 73</p>
       </div>
       <div class="item">
+        <!--
        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10637.899116033648!2d3.2748341!3d48.1974703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe4dbf7fd9bd0c7eb!2sLe%20Celtic%20Bar!5e0!3m2!1sfr!2sfr!4v1667213362310!5m2!1sfr!2sfr" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      -->
       </div>
      </div>
     </div>
