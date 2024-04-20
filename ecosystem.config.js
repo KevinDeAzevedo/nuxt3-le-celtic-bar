634d893fe8c10c4e17155ff60bdd5921160b0d90
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2024-04-21 00:19:37 +0200

    only one instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'Nuxt3-Le-Celtic-Bar-1347',
       exec_mode: 'cluster',
-      instances: 'max',
+      instances: '1',
       env: {
         PORT: 1347,
         NITRO_HOST: '127.0.0.1',
